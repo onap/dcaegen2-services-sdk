ed1e6cf56fcd89eaff3cb9665c256c0a2a86debd
Zlatko Murgoski <zlatko.murgoski@nokia.com>
2018-12-06 15:39:06 +0100

    Tool for password cryptography
    Tool for password cryptography
    
    Change-Id: Ifad00c8ca411f9f68535396aac1266fbe1951e51
    Issue-ID: DCAEGEN2-1020
    Signed-off-by: Zlatko Murgoski <zlatko.murgoski@nokia.com>

---
+++ crypt-password/README.md
@@ -0,0 +1,10 @@
+Crypt Password 
+==================================
+
+Util to generate cryptography password using BCrypt algorithm
+
+### Requirements 
+pip install -r requirements.txt
+
+### How to use
+python crypt_password.py -p password_to_crypt 
---
+++ crypt-password/crypt_password.py
@@ -0,0 +1,11 @@
+import bcrypt
+import argparse
+
+def crypt(password):
+    return bcrypt.hashpw(password, bcrypt.gensalt(rounds = 10, prefix=b"2a"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', dest='password', help="Generate new crypt password using -p flag")
+    result=parser.parse_args()
+    print(crypt(result.password))
