7b020e0347a5fc48e6be4cf82764ac2df600c5c6
pbiniek <pawel.biniek@nokia.com>
2019-02-01 14:10:05 +0100

    Simple OK case for HV-VES
    * includes building of actual message (only some fields set)
    * includes receiving it, and validates if fields are properly decoded
    
    Change-Id: Ic20afa1487e24ff42db399bd68ea490744c193e9
    Issue-ID: DCAEGEN2-1069
    Signed-off-by: Pawel Biniek <pawel.biniek@nokia.com>

--- services/hv-ves-client/producer/ct/src/test/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/ct/HvVesProducerIT.java
+++ services/hv-ves-client/producer/ct/src/test/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/ct/HvVesProducerIT.java
@@ -19,21 +19,28 @@
  */
 package org.onap.dcaegen2.services.sdk.services.hvves.client.producer.ct;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.google.protobuf.ByteString;
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.onap.ves.MeasDataCollectionOuterClass;
 import org.onap.ves.VesEventOuterClass.CommonEventHeader;
 import org.onap.ves.VesEventOuterClass.VesEvent;
 import reactor.core.publisher.Flux;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author <a href="mailto:piotr.jaszczyk@nokia.com">Piotr Jaszczyk</a>
  */
 class HvVesProducerIT {
+
+    private static final int INFO_ID = 17;
+    private static final long VALUE = 5l;
+    private static final int MEAS_TYPE = 3;
+    private static final int PERIOD = 1000;
+    private static final String OBJECT_INSTANCE_ID = "DH-1";
 
     private final SystemUnderTestWrapper sut = new SystemUnderTestWrapper();
 
@@ -48,23 +55,63 @@ class HvVesProducerIT {
     }
 
     @Test
-    void todo() {
+    void singleMessageTest() throws Exception {
         // given
-        final VesEvent sampleEvent = VesEvent.newBuilder()
-                .setCommonEventHeader(CommonEventHeader.newBuilder()
-                        .setDomain("dummy")
-                        .build())
-                .setEventFields(ByteString.copyFrom(new byte[]{0, 1, 2, 3}))
-                .build();
 
+        final VesEvent sampleEvent = createSimpleVesEvent();
         final Flux<VesEvent> input = Flux.just(sampleEvent);
 
         // when
         final ByteBuf receivedData = sut.blockingSend(input);
 
         // then
-        assertThat(receivedData.readableBytes())
-                .describedAs("data length")
-                .isGreaterThan(0);
+
+        // check wire protocol
+        byte[] wireProtocolHeader = new byte[6];
+        receivedData.readBytes(wireProtocolHeader);
+        short type = receivedData.readShort();
+        assertThat(type).isEqualTo((short)0x0001);
+
+        // check common header
+        int payloadSize = receivedData.readInt();
+        byte[] buffer = new byte[payloadSize];
+        receivedData.readBytes(buffer);
+        VesEvent result = VesEvent.parseFrom(buffer);
+        assertThat(result.getCommonEventHeader().getDomain()).isEqualTo("RTPM");
+
+        // check payload
+        MeasDataCollectionOuterClass.MeasDataCollection content =
+                MeasDataCollectionOuterClass.MeasDataCollection.parseFrom(result.getEventFields());
+        assertThat(content.getGranularityPeriod()).isEqualTo(PERIOD);
+        assertThat(content.getMeasObjInstIdListList()).containsExactly(OBJECT_INSTANCE_ID);
+        assertThat(content.getMeasObjInstIdListList().size()).isEqualTo(1);
+        MeasDataCollectionOuterClass.MeasInfo info = content.getMeasInfo(0);
+        assertThat(info.getIMeasInfoId()).isEqualTo(INFO_ID);
+        assertThat(info.getIMeasTypes().getIMeasTypeList()).containsExactly(MEAS_TYPE);
+        assertThat(info.getMeasValuesList().size()).isEqualTo(1);
+        assertThat(info.getMeasValues(0).getMeasResultsList().size()).isEqualTo(1);
+        assertThat(info.getMeasValues(0).getMeasResults(0).getIValue()).isEqualTo(VALUE);
+    }
+
+    private VesEvent createSimpleVesEvent() {
+        final MeasDataCollectionOuterClass.MeasDataCollection content = MeasDataCollectionOuterClass.MeasDataCollection.newBuilder()
+                .addMeasInfo(MeasDataCollectionOuterClass.MeasInfo.newBuilder()
+                        .addMeasValues(MeasDataCollectionOuterClass.MeasValue.newBuilder()
+                                .addMeasResults(MeasDataCollectionOuterClass.MeasResult.newBuilder()
+                                        .setIValue(VALUE).build())
+                                .build())
+                        .setIMeasInfoId(INFO_ID)
+                        .setIMeasTypes(MeasDataCollectionOuterClass.MeasInfo.IMeasTypes.newBuilder()
+                                .addIMeasType(MEAS_TYPE))
+                        .build())
+                .setGranularityPeriod(PERIOD)
+                .addMeasObjInstIdList(OBJECT_INSTANCE_ID)
+                .build();
+        return VesEvent.newBuilder()
+                .setCommonEventHeader(CommonEventHeader.newBuilder()
+                        .setDomain("RTPM")
+                        .build())
+                .setEventFields(content.toByteString())
+                .build();
     }
 }
