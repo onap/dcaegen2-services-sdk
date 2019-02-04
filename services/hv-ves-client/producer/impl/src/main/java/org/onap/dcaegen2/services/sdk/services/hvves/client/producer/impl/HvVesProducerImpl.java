16cbac1859162e4207f80895885942b37b767306
Jakub Dudycz <jakub.dudycz@nokia.com>
2019-02-04 15:18:28 +0100

    Add send with raw payload method to HvVesProducer
    Change-Id: I430b176373c8c351105c1d10047aace63319dd7c
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>
    Issue-ID: DCAEGEN2-1164

--- services/hv-ves-client/producer/impl/src/main/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/impl/HvVesProducerImpl.java
+++ services/hv-ves-client/producer/impl/src/main/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/impl/HvVesProducerImpl.java
@@ -19,11 +19,15 @@
  */
 package org.onap.dcaegen2.services.sdk.services.hvves.client.producer.impl;
 
+import io.netty.buffer.ByteBuf;
+import java.nio.ByteBuffer;
+import java.util.function.BiFunction;
 import org.jetbrains.annotations.NotNull;
 import org.onap.dcaegen2.services.sdk.services.hvves.client.producer.api.HvVesProducer;
+import org.onap.dcaegen2.services.sdk.services.hvves.client.producer.api.options.PayloadType;
 import org.onap.ves.VesEventOuterClass.VesEvent;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
+import reactor.netty.NettyInbound;
 import reactor.netty.NettyOutbound;
 import reactor.netty.tcp.TcpClient;
 
@@ -36,23 +40,40 @@ public class HvVesProducerImpl implements HvVesProducer {
     private final TcpClient tcpClient;
     private final ProducerCore producerCore;
 
-
     HvVesProducerImpl(TcpClient tcpClient, ProducerCore producerCore) {
         this.tcpClient = tcpClient;
         this.producerCore = producerCore;
     }
 
     @Override
-    public @NotNull Mono<Void> send(Publisher<VesEvent> messages) {
+    public @NotNull Publisher<Void> send(Publisher<VesEvent> messages) {
+        return handleConnection((in, out) -> handle(messages, out));
+    }
+
+    @Override
+    public @NotNull Publisher<Void> sendRaw(Publisher<ByteBuffer> messages, PayloadType payloadType) {
+        return handleConnection((in, out) -> handleRaw(messages, payloadType, out));
+    }
+
+    private Publisher<Void> handleConnection(
+            BiFunction<NettyInbound, NettyOutbound, Publisher<Void>> handler) {
+
         return tcpClient
-            .handle((in, out) -> handle(messages, out))
-            .connect()
-            .then();
+                .handle(handler)
+                .connect()
+                .then();
     }
 
     private Publisher<Void> handle(Publisher<VesEvent> messages, NettyOutbound outbound) {
-        return outbound
-            .send(producerCore.encode(messages, outbound.alloc()))
-            .then();
+        return push(producerCore.encode(messages, outbound.alloc()), outbound);
+    }
+
+    private Publisher<Void> handleRaw(Publisher<ByteBuffer> messages, PayloadType payloadType,
+            NettyOutbound outbound) {
+        return push(producerCore.encode(messages, payloadType, outbound.alloc()), outbound);
+    }
+
+    private Publisher<Void> push(Publisher<ByteBuf> messages, NettyOutbound outbound) {
+        return outbound.send(messages).then();
     }
 }
