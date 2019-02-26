4b1062c01ea444f429ca7c51153c2cea5692ed80
Piotr Jaszczyk <piotr.jaszczyk@nokia.com>
2019-02-26 15:08:37 +0100

    CBS lookup algorithm
    Implement CBS lookup algorithm as docummented on
    https://wiki.onap.org/display/DW/MicroServices+Onboarding+in+ONAP#MicroServicesOnboardinginONAP-CodesnippettofetchconfigurationfromConfigbindingservice
    
    Change-Id: Ib465c5fd44b853ba46e152259744dbdd775872a0
    Issue-ID: DCAEGEN2-1233
    Signed-off-by: Piotr Jaszczyk <piotr.jaszczyk@nokia.com>

---
+++ rest-services/cbs-client/src/main/java/org/onap/dcaegen2/services/sdk/rest/services/cbs/client/impl/CbsLookup.java
@@ -0,0 +1,72 @@
+/*
+ * ============LICENSE_START====================================
+ * DCAEGEN2-SERVICES-SDK
+ * =========================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * =========================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================
+ */
+
+package org.onap.dcaegen2.services.sdk.rest.services.cbs.client.impl;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.net.InetSocketAddress;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.api.EnvProperties;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.impl.adapters.CloudHttpClient;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author <a href="mailto:piotr.jaszczyk@nokia.com">Piotr Jaszczyk</a>
+ * @since February 2019
+ */
+public class CbsLookup {
+
+    private static final String CONSUL_JSON_SERVICE_ADDRESS = "ServiceAddress";
+    private static final String CONSUL_JSON_SERVICE_PORT = "ServicePort";
+    private final CloudHttpClient httpClient;
+
+    public CbsLookup(CloudHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public Mono<InetSocketAddress> lookup(EnvProperties env) {
+        return Mono.fromCallable(() -> createConsulUrl(env))
+                .flatMap(this::fetchHttpData)
+                .flatMap(this::firstService)
+                .map(this::parseServiceEntry);
+    }
+
+    private String createConsulUrl(EnvProperties env) {
+        return String.format("http://%s:%s/v1/catalog/service/%s", env.consulHost(), env.consulPort(), env.cbsName());
+    }
+
+    private Mono<JsonArray> fetchHttpData(String consulUrl) {
+        return httpClient.callHttpGet(consulUrl, JsonArray.class);
+    }
+
+    private Mono<JsonObject> firstService(JsonArray services) {
+        return services.size() == 0
+                ? Mono.empty()
+                : Mono.just(services.get(0).getAsJsonObject());
+    }
+
+    private InetSocketAddress parseServiceEntry(JsonObject service) {
+        return InetSocketAddress.createUnresolved(
+                service.get(CONSUL_JSON_SERVICE_ADDRESS).getAsString(),
+                service.get(CONSUL_JSON_SERVICE_PORT).getAsInt());
+    }
+
+}
+
