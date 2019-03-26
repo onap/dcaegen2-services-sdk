e12c8567b06fac6b2326d068c30b087f0e3fd51c
Piotr Jaszczyk <piotr.jaszczyk@nokia.com>
2019-03-26 11:40:29 +0100

    Proposal of enhanced DMaaP API
    Change-Id: Ia941489aaa3ee34c42cedd719a957ec2a2ea7181
    Issue-ID: DCAEGEN2-1368
    Signed-off-by: Piotr Jaszczyk <piotr.jaszczyk@nokia.com>

---
+++ rest-services/dmaap-client/src/main/java/org/onap/dcaegen2/services/sdk/rest/services/dmaap/client/api/DmaapClientFactory.java
@@ -0,0 +1,39 @@
+/*
+ * ============LICENSE_START====================================
+ * DCAEGEN2-SERVICES-SDK
+ * =========================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * =========================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================
+ */
+
+package org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.api;
+
+/**
+ * @author <a href="mailto:piotr.jaszczyk@nokia.com">Piotr Jaszczyk</a>
+ * @since March 2019
+ */
+public final class DmaapClientFactory {
+
+    private DmaapClientFactory() {
+    }
+
+    public static DmaapPublisher createPublisher() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public static DmaapSubscriber createSubscriber() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
