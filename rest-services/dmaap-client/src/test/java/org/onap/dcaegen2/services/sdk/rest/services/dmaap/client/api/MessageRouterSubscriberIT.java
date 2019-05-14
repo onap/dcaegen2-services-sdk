af26a0c853c5a5817115c79f9193a04009d2fc94
Izabela Zawadzka <izabela.zawadzka@nokia.com>
2019-05-14 09:05:49 +0200

    Add tests for MR Subscriber
    Change-Id: I705bcd5c0a75d9ac637afdb8cd6ac17b0af55c78
    Issue-ID: DCAEGEN2-1422
    Signed-off-by: Izabela Zawadzka <izabela.zawadzka@nokia.com>

--- rest-services/dmaap-client/src/test/java/org/onap/dcaegen2/services/sdk/rest/services/dmaap/client/api/MessageRouterSubscriberIT.java
+++ rest-services/dmaap-client/src/test/java/org/onap/dcaegen2/services/sdk/rest/services/dmaap/client/api/MessageRouterSubscriberIT.java
@@ -20,8 +20,10 @@
 
 package org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.api;
 
+import static org.onap.dcaegen2.services.sdk.rest.services.adapters.http.test.DummyHttpServer.sendError;
 import static org.onap.dcaegen2.services.sdk.rest.services.adapters.http.test.DummyHttpServer.sendResource;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import java.time.Duration;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,9 +32,12 @@ import org.onap.dcaegen2.services.sdk.model.streams.dmaap.ImmutableMessageRouter
 import org.onap.dcaegen2.services.sdk.model.streams.dmaap.MessageRouterSource;
 import org.onap.dcaegen2.services.sdk.rest.services.adapters.http.test.DummyHttpServer;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.ImmutableMessageRouterSubscribeRequest;
+import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.ImmutableMessageRouterSubscribeResponse;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterSubscribeRequest;
+import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterSubscribeResponse;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.config.MessageRouterSubscriberConfig;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 /**
@@ -40,36 +45,130 @@ import reactor.test.StepVerifier;
  * @since May 2019
  */
 class MessageRouterSubscriberIT {
-    private MessageRouterSubscriber sut = DmaapClientFactory.createMessageRouterSubscriber(MessageRouterSubscriberConfig.createDefault());
-    private static DummyHttpServer server;
-    private static MessageRouterSource sourceDefinition;
+    private static final Duration TEN_SECONDS = Duration.ofSeconds(10);
+    private static final String CONSUMER_GROUP = "group1";
+    private static final String SUCCESS_CONSUMER_ID = "consumer8";
+    private static final String FAILING_CONSUMER_ID = "consumer12";
+    private static final String SUCCESS_RESP_PATH = String
+            .format("/events/TOPIC/%s/%s", CONSUMER_GROUP, SUCCESS_CONSUMER_ID);
+    private static final String FAILING_RESP_PATH = String
+            .format("/events/TOPIC/%s/%s", CONSUMER_GROUP, FAILING_CONSUMER_ID);
+
+    private static MessageRouterSubscribeRequest mrSuccessRequest;
+    private static MessageRouterSubscribeRequest mrFailingRequest;
+    private MessageRouterSubscriber sut = DmaapClientFactory
+            .createMessageRouterSubscriber(MessageRouterSubscriberConfig.createDefault());
+
 
     @BeforeAll
     static void setUp() {
-        server = DummyHttpServer.start(routes ->
-                routes.get("/events/TOPIC/group1/consumer8", (req, resp) -> sendResource(resp, "/sample-mr-subscribe-response.json"))
+        DummyHttpServer server = DummyHttpServer.start(routes ->
+                routes
+                        .get(SUCCESS_RESP_PATH, (req, resp) ->
+                                sendResource(resp, "/sample-mr-subscribe-response.json"))
+                        .get(FAILING_RESP_PATH, (req, resp) ->
+                                sendError(resp, 500, "Something went wrong"))
         );
-        sourceDefinition = ImmutableMessageRouterSource.builder()
+
+        MessageRouterSource sourceDefinition = ImmutableMessageRouterSource.builder()
                 .name("the topic")
                 .topicUrl(String.format("http://%s:%d/events/TOPIC", server.host(), server.port()))
+                .build();
+
+        mrSuccessRequest = ImmutableMessageRouterSubscribeRequest.builder()
+                .sourceDefinition(sourceDefinition)
+                .consumerGroup(CONSUMER_GROUP)
+                .consumerId(SUCCESS_CONSUMER_ID)
+                .build();
+
+        mrFailingRequest = ImmutableMessageRouterSubscribeRequest
+                .builder()
+                .sourceDefinition(sourceDefinition)
+                .consumerGroup(CONSUMER_GROUP)
+                .consumerId(FAILING_CONSUMER_ID)
                 .build();
     }
 
     @Test
-    void testStub() {
-        final MessageRouterSubscribeRequest mrRequest = ImmutableMessageRouterSubscribeRequest.builder()
-                .sourceDefinition(sourceDefinition)
-                .consumerGroup("group1")
-                .consumerId("consumer8")
+    void subscriber_shouldGetCorrectResponse(){
+        Mono<MessageRouterSubscribeResponse> response = sut
+                .get(mrSuccessRequest);
+
+        JsonArray expectedItems = new JsonArray();
+        expectedItems.add("I");
+        expectedItems.add("like");
+        expectedItems.add("pizza");
+
+        ImmutableMessageRouterSubscribeResponse expectedResponse = ImmutableMessageRouterSubscribeResponse
+                .builder()
+                .items(expectedItems)
                 .build();
 
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify(TEN_SECONDS);
+    }
+
+    @Test
+    void subscriber_shouldGetErrorResponse(){
+        Mono<MessageRouterSubscribeResponse> response = sut
+                .get(mrFailingRequest);
+
+        ImmutableMessageRouterSubscribeResponse expectedResponse = ImmutableMessageRouterSubscribeResponse
+                .builder()
+                .failReason("500 Internal Server Error\n" + "Something went wrong")
+                .build();
+
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify(TEN_SECONDS);
+    }
+
+    @Test
+    void subscriber_shouldParseCorrectResponse() {
         final Flux<String> result = sut
-                .getElements(mrRequest)
+                .getElements(mrSuccessRequest)
                 .map(JsonElement::getAsString);
 
         StepVerifier.create(result)
                 .expectNext("I", "like", "pizza")
                 .expectComplete()
-                .verify(Duration.ofSeconds(10));
+                .verify(TEN_SECONDS);
+    }
+
+    @Test
+    void subscriber_shouldParseErrorResponse(){
+        Flux<String> result = sut
+                .getElements(mrFailingRequest)
+                .map(JsonElement::getAsString);
+
+        StepVerifier.create(result)
+                .expectError(IllegalStateException.class)
+                .verify(TEN_SECONDS);
+    }
+
+    @Test
+    void subscriber_shouldSubscribeCorrectly(){
+        Flux<String> subscriptionForElements = sut
+                .subscribeForElements(mrSuccessRequest, Duration.ofSeconds(1))
+                .map(JsonElement:: getAsString);
+
+        StepVerifier.create(subscriptionForElements.take(2))
+                .expectNext("I", "like")
+                .expectComplete()
+                .verify(TEN_SECONDS);
+    }
+
+    @Test
+    void subscriber_shouldParseErrorWhenSubscribed(){
+        Flux<String> subscriptionForElements = sut
+                .subscribeForElements(mrFailingRequest, Duration.ofSeconds(1))
+                .map(JsonElement:: getAsString);
+
+        StepVerifier.create(subscriptionForElements.take(2))
+                .expectError(IllegalStateException.class)
+                .verify(TEN_SECONDS);
     }
 }
