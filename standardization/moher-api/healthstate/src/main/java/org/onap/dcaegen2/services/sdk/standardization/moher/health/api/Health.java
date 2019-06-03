7c6fad3ec1a35e1b2b31c2f3d9ac77e267f99dce
Piotr Jaszczyk <piotr.jaszczyk@nokia.com>
2019-06-03 14:20:40 +0200

    Monitoring API - write framework adapters
    Change-Id: Iaba9c4ef6022d01c1f572055316700a3a4dfa8f2
    Issue-ID: DCAEGEN2-1589
    Signed-off-by: Piotr Jaszczyk <piotr.jaszczyk@nokia.com>

---
+++ standardization/moher-api/healthstate/src/main/java/org/onap/dcaegen2/services/sdk/standardization/moher/health/api/Health.java
@@ -0,0 +1,39 @@
+/*
+ * ============LICENSE_START====================================
+ * DCAEGEN2-SERVICES-SDK
+ * =========================================================
+ * Copyright (C) 2019 Nokia. All rights reserved.
+ * =========================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================
+ */
+
+package org.onap.dcaegen2.services.sdk.standardization.moher.health.api;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Gson.TypeAdapters
+public interface Health {
+
+    @Value.Default
+    default boolean healthy() {
+        return true;
+    }
+
+    @Value.Default
+    default String description() {
+        return "Service is working correctly";
+    }
+}
