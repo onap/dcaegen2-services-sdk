880c65da340289a011af615e40c471c765d0c3fb
kurczews <krzysztof.kurczewski@nokia.com>
2019-06-10 14:39:01 +0200

    Refactor aai configuration urls
    Issue-ID: DCAEGEN2-1608
    Change-Id: I4efdcdcde4e3905e369cec171bcd7e629e1b7bee
    Signed-off-by: kurczews <krzysztof.kurczewski@nokia.com>

--- rest-services/aai-client/src/main/java/org/onap/dcaegen2/services/sdk/rest/services/aai/client/config/AaiClientConfiguration.java
+++ rest-services/aai-client/src/main/java/org/onap/dcaegen2/services/sdk/rest/services/aai/client/config/AaiClientConfiguration.java
@@ -20,22 +20,32 @@
 
 package org.onap.dcaegen2.services.sdk.rest.services.aai.client.config;
 
-import org.immutables.gson.Gson;
-import org.immutables.value.Value;
-
 import java.io.Serializable;
 import java.util.Map;
-
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
 
 @Value.Immutable(prehash = true)
 @Value.Style(builder = "new")
 @Gson.TypeAdapters
 public abstract class AaiClientConfiguration implements Serializable {
 
+    private static final String PNF_PATH = "/network/pnfs/pnf";
+    private static final String SERVICE_INSTANCE_PATH = "/business/customers/customer/${customer}/service-subscriptions/service-subscription/${serviceType}/service-instances/service-instance/${serviceInstanceId}";
+
     private static final long serialVersionUID = 1L;
 
     @Value.Parameter
-    public abstract String pnfUrl();
+    public abstract String baseUrl();
+
+    /**
+     * Please use baseUrl() instead
+     */
+    @Deprecated
+    @Value.Default
+    public String pnfUrl() {
+        return baseUrl() + PNF_PATH;
+    }
 
     @Value.Parameter
     public abstract String aaiUserName();
@@ -46,8 +56,14 @@ public abstract class AaiClientConfiguration implements Serializable {
     @Value.Parameter
     public abstract Boolean aaiIgnoreSslCertificateErrors();
 
-    @Value.Parameter
-    public abstract String aaiServiceInstancePath();
+    /**
+     * Please use baseUrl() instead
+     */
+    @Deprecated
+    @Value.Default
+    public String aaiServiceInstancePath() {
+        return SERVICE_INSTANCE_PATH;
+    }
 
     @Value.Parameter
     public abstract Map<String, String> aaiHeaders();
@@ -66,5 +82,4 @@ public abstract class AaiClientConfiguration implements Serializable {
 
     @Value.Parameter
     public abstract Boolean enableAaiCertAuth();
-
 }
