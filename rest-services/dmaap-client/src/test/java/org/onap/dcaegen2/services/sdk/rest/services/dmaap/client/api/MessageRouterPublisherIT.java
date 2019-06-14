8d82a8b2f882d331fb98de99bfefed24c6c5202a
Izabela Zawadzka <izabela.zawadzka@nokia.com>
2019-06-14 15:26:29 +0200

    Add text/plain content type handling in Publisher
    Change-Id: I51e17d64f813e16b81385abb8aa862ee1f927d35
    Signed-off-by: Izabela Zawadzka <izabela.zawadzka@nokia.com>
    Issue-ID: DCAEGEN2-1630

--- rest-services/dmaap-client/src/test/java/org/onap/dcaegen2/services/sdk/rest/services/dmaap/client/api/MessageRouterPublisherIT.java
+++ rest-services/dmaap-client/src/test/java/org/onap/dcaegen2/services/sdk/rest/services/dmaap/client/api/MessageRouterPublisherIT.java
@@ -20,15 +20,15 @@
 
 package org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.api;
 
-import static org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.api.MessageRouterTestsUtils.*;
+import static org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.MessageRouterTestsUtils.*;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import io.vavr.collection.List;
 import java.time.Duration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.ContentType;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterPublishRequest;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterPublishResponse;
 import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterSubscribeRequest;
@@ -95,7 +95,7 @@ class MessageRouterPublisherIT {
         //given
         final String topic = "TOPIC2";
         final List<String> threePlainTextMessages = List.of("I", "like", "pizza");
-        final Flux<JsonPrimitive> messageBatch = plainBatch(threePlainTextMessages);
+        final Flux<JsonElement> messageBatch = plainBatch(threePlainTextMessages);
         final MessageRouterPublishRequest mrRequest = createPublishRequest(String.format("%s/%s", EVENTS_PATH, topic));
         final MessageRouterPublishResponse expectedResponse = errorPublishResponse(
                 DMAAP_400_ERROR_RESPONSE_FORMAT, topic);
@@ -112,6 +112,7 @@ class MessageRouterPublisherIT {
 
     @Test
     void publisher_shouldSuccessfullyPublishSingleMessage(){
+        //given
         final String topic = "TOPIC3";
         final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
         final List<String> singleJsonMessage = List.of("{\"message\":\"message1\"}");
@@ -136,7 +137,7 @@ class MessageRouterPublisherIT {
 
     @Test
     void publisher_shouldSuccessfullyPublishMultipleMessages(){
-        final String topic = "TOPIC4";
+        final String topic = "TOPIC5";
         final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
         final List<String> singleJsonMessage = List.of("{\"message\":\"message1\"}",
                 "{\"differentMessage\":\"message2\"}");
@@ -150,6 +151,114 @@ class MessageRouterPublisherIT {
         registerTopic(publisher, publishRequest, subscriber, subscribeRequest);
         Mono<MessageRouterSubscribeResponse> response = publisher
                 .put(publishRequest, jsonMessageBatch)
+                .then(subscriber.get(subscribeRequest));
+
+        //then
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void publisher_shouldSuccessfullyPublishSingleJsonMessageWithPlainContentType(){
+        //given
+        final String topic = "TOPIC6";
+        final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
+
+        final List<String> singleJsonMessage = List.of("{\"message\":\"message1\"}");
+        final List<JsonElement> expectedItems = getAsJsonElements(singleJsonMessage);
+        final Flux<JsonElement> plainBatch = plainBatch(singleJsonMessage);
+
+        final MessageRouterPublishRequest publishRequest = createPublishRequest(topicUrl, ContentType.TEXT_PLAIN);
+        final MessageRouterSubscribeRequest subscribeRequest = createMRSubscribeRequest(topicUrl, "sampleGroup", "sampleId");
+        final MessageRouterSubscribeResponse expectedResponse = successSubscribeResponse(expectedItems);
+
+        //when
+        registerTopic(publisher, publishRequest, subscriber, subscribeRequest);
+        Mono<MessageRouterSubscribeResponse> response = publisher
+                .put(publishRequest, plainBatch)
+                .then(subscriber.get(subscribeRequest));
+
+        //then
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void publisher_shouldSuccessfullyPublishMultipleJsonMessagesWithPlainContentType(){
+        //given
+        final String topic = "TOPIC7";
+        final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
+
+        final List<String> twoJsonMessage = List.of("{\"message\":\"message1\"}", "{\"message2\":\"message2\"}");
+        final List<JsonElement> expectedItems = getAsJsonElements(twoJsonMessage);
+        final Flux<JsonElement> plainBatch = plainBatch(twoJsonMessage);
+
+        final MessageRouterPublishRequest publishRequest = createPublishRequest(topicUrl, ContentType.TEXT_PLAIN);
+        final MessageRouterSubscribeRequest subscribeRequest = createMRSubscribeRequest(topicUrl, "sampleGroup", "sampleId");
+        final MessageRouterSubscribeResponse expectedResponse = successSubscribeResponse(expectedItems);
+
+        //when
+        registerTopic(publisher, publishRequest, subscriber, subscribeRequest);
+        Mono<MessageRouterSubscribeResponse> response = publisher
+                .put(publishRequest, plainBatch)
+                .then(subscriber.get(subscribeRequest));
+
+        //then
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void publisher_shouldSuccessfullyPublishSinglePlainMessageWithPlainContentType(){
+        //given
+        final String topic = "TOPIC8";
+        final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
+
+        final List<String> singlePlainMessage = List.of("kebab");
+        final List<JsonElement> expectedItems = getAsJsonElements(singlePlainMessage);
+        final Flux<JsonElement> plainBatch = plainBatch(singlePlainMessage);
+
+        final MessageRouterPublishRequest publishRequest = createPublishRequest(topicUrl, ContentType.TEXT_PLAIN);
+        final MessageRouterSubscribeRequest subscribeRequest = createMRSubscribeRequest(topicUrl, "sampleGroup", "sampleId");
+        final MessageRouterSubscribeResponse expectedResponse = successSubscribeResponse(expectedItems);
+
+        //when
+        registerTopic(publisher, publishRequest, subscriber, subscribeRequest);
+        Mono<MessageRouterSubscribeResponse> response = publisher
+                .put(publishRequest, plainBatch)
+                .then(subscriber.get(subscribeRequest));
+
+        //then
+        StepVerifier.create(response)
+                .expectNext(expectedResponse)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void publisher_shouldSuccessfullyPublishMultiplePlainMessagesWithPlainContentType(){
+        //given
+        final String topic = "TOPIC9";
+        final String topicUrl = String.format("%s/%s", EVENTS_PATH, topic);
+
+        final List<String> singlePlainMessage = List.of("I", "like", "pizza");
+        final List<JsonElement> expectedItems = getAsJsonElements(singlePlainMessage);
+        final Flux<JsonElement> plainBatch = plainBatch(singlePlainMessage);
+
+        final MessageRouterPublishRequest publishRequest = createPublishRequest(topicUrl, ContentType.TEXT_PLAIN);
+        final MessageRouterSubscribeRequest subscribeRequest = createMRSubscribeRequest(topicUrl, "sampleGroup", "sampleId");
+        final MessageRouterSubscribeResponse expectedResponse = successSubscribeResponse(expectedItems);
+
+        //when
+        registerTopic(publisher, publishRequest, subscriber, subscribeRequest);
+        Mono<MessageRouterSubscribeResponse> response = publisher
+                .put(publishRequest, plainBatch)
                 .then(subscriber.get(subscribeRequest));
 
         //then
