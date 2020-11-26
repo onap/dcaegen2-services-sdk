ea4a1af6d1afdd71a635ab99d1cb685b8fcda9f6
tkogut <tomasz.kogut@nokia.com>
2020-11-26 09:31:32 +0100

    Update Reactor version and upgrade TestContainers
    Issue-ID: DCAEGEN2-2532
    Signed-off-by: tkogut <tomasz.kogut@nokia.com>
    Change-Id: Ic92bdbb7b82055e7b9ec211d50d04f6843b4ca5e

--- services/hv-ves-client/producer/ct/src/test/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/ct/DummyCollector.java
+++ services/hv-ves-client/producer/ct/src/test/java/org/onap/dcaegen2/services/sdk/services/hvves/client/producer/ct/DummyCollector.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * DCAEGEN2-SERVICES-SDK
  * ================================================================================
- * Copyright (C) 2019 Nokia. All rights reserved.
+ * Copyright (C) 2020 Nokia. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,7 +67,7 @@ public class DummyCollector {
                         .wiretap(true)
                         .handle(this::handleConnection);
         server = tcpServer.bindNow();
-        return server.address();
+        return (InetSocketAddress) server.address();
     }
 
     public void stop() {
